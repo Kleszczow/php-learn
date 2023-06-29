a968925238f0704f2fbcf5ee32dcc1416c2b7592
Tymek Slomski <tymekslomski@gmail.com>
2023-06-29 12:23:12 +0200

    same new stuff

--- index.php
+++ index.php
@@ -11,6 +11,8 @@
     <input type="text" name="username"><br>
     <label>Password:</label><br>
     <input type="password" name="password"><br>
+    <label>Country:</label><br>
+    <input type="text" name="country"><br>
     <input type="submit" value="Log In">
     </form>
 </body>
@@ -35,9 +37,57 @@
     $total = null;
     $total = $value * $price;
     echo"You have {$value} of snickers<br> Your total is \${$total} <br>";*/
+    $password = $_POST["password"];
+    $passLenght = strlen($password);
     echo "{$_POST["username"]} <br>";
-    echo "{$_POST["password"]} <br>";
-
+    echo "{$password} <br>";
     //get pokazuje w url wartości
     //post nie pokazuje!!!
+    if(empty($password)){
+        echo "not decLARATED PASSWORD";
+} else {
+    if( $passLenght <= 4){
+        echo"password is too short! <br>";
+    } elseif($passLenght == 5){
+        echo"password is medium strong <br>";
+    }
+    else {
+        echo"password is strong <br>";
+    }
+};
+
+   
+
+    $capitals = array("USA" => "Washington D.C", "Japan" => "Tokio", "Poland" => "Warsaw",);
+  /*  $capitals["Poland"] = "Gliwice";
+    //array_pop($capitals); - usuwa ostatnia shift usuwa pierwsza
+    $capitals["England"] = "London";
+    $keys = array_keys($capitals);
+    foreach($keys as $capital){
+        echo"capital {$capital} <br>";
+    }
+    $values = array_values($capitals);
+    foreach($values as $city){
+        echo"city {$city} <br>";
+    }
+
+
+    //array_reverse obraca nam góra dół
+    //array_flip obraca nam składniki 
+   // echo"<br>{$capitals["USA"]} <br>";
+    foreach($capitals as $key => $value) {
+        echo"{$key} is {$value} <br>";
+    }
+    $counter = count($capitals);
+    echo"counter {$counter} <br>";
+    */
+    $capital = $capitals[$_POST["country"]];
+    echo$capital;
+
+    $thisValue = "lol";
+    if(empty($thisValue)){
+        echo"rain";
+    } else {
+        echo"sun";
+    };
 ?>
